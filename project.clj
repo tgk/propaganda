b1352d92103d856c5b36d02c1af8956e201dd89d
Thomas Kristensen <thomas.kristensen@uswitch.com>
2013-06-25 12:18:51 +0100

    Add Clojureized version of Section 2 of "The Art of the Propagator"

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject propaganda "0.0.1-SNAPSHOT"
+  :description "(Hopefully) extensible propagator library"
+  :url "http://github.com/tgk/propaganda"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
