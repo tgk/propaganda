2ea1d4bf91ff82ef322bb19d47a255e8a735a410
Thomas Kristensen <thomas.kristensen@uswitch.com>
2013-07-02 13:19:43 +0100

    Add lein release plugin

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://github.com/tgk/propaganda"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :plugins [[lein-release "1.0.4"]]
+  :lein-release {:scm :git
+                 :deploy-via :shell
+                 :shell ["lein" "deploy" "clojars"]})
