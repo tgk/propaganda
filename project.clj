a68793f8bf83f7a7aa73a4898c05c5589cdca851
Thomas G. Kristensen <thomas.g.kristensen@gmail.com>
2013-09-07 11:12:05 +0100

    Implement all basic functions needed for propagation in system approach.

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :url "http://github.com/tgk/propaganda"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+
+                 ;; convenient for debugging
+                 [dire "0.4.4"]
+
+                 ]
   :plugins [[lein-release "1.0.4"]]
   :lein-release {:scm :git
                  :deploy-via :shell
--- src/propaganda/system.clj
+++ src/propaganda/system.clj
@@ -1,19 +1,17 @@
 (ns propaganda.system
-  (:require [propaganda.generic-operators :as generic-operators]))
+  (:require [propaganda.generic-operators :as generic-operators])
+  (:require [dire.core :refer [with-pre-hook! with-post-hook!]]))
 
 (defrecord PropagatorSystem
-    [values propagators merge contradictory? alert-queue])
+    [values propagators merge contradictory? alert-queue freezing?])
 
 (defprotocol PropagatorSystemProtocol
-  (make-cell      [this])
-  (add-value      [this cell value])
-  (get-value      [this cell])
-  (add-propagator [this cells f])
-  (cool           [this])
-  (stabile?       [this])
-
-  (-alert-propagators     [this fs])
-  (-alert-all-propagators [this]))
+  (add-value             [this cell value])
+  (get-value             [this cell])
+  (add-propagator        [this cells f])
+  (cool                  [this])
+  (stabile?              [this])
+  (alert-all-propagators [this]))
 
 ;;; START: Stuff that is independent of propagator system implementation strategy
 
@@ -69,10 +67,121 @@
 
 ;;;;;; STOP
 
+#_(defrecord PropagatorSystem
+    [values propagators merge contradictory? alert-queue])
 
+#_(defprotocol PropagatorSystemProtocol
+  (add-value      [this cell value])
+  (get-value      [this cell])
+  (add-propagator [this cells f])
+  (cool           [this])
+  (stabile?       [this])
 
+  (-alert-propagators     [this fs])
+  (-alert-all-propagators [this]))
+
+(defn update-keys
+  [m ks f & args]
+  (reduce (fn [m k] (assoc m k (apply f (get m k) args)))
+          m
+          ks))
+
+(defn- disturb-system
+  "Helper for adding a value to the system, without cooling it."
+  [system cell value]
+  (let [content (get-value system cell)
+        answer ((:merge system) content value)]
+    (cond
+
+     (= answer content)
+     system
+
+     ((:contradictory? system) answer)
+     (throw (ex-info "Inconsistency" {:contradiction answer}))
+
+     :else
+     (-> system
+         (assoc-in [:values cell] answer)
+         (update-in [:alert-queue] concat (get-in system [:propagators cell]))))))
+
+;; freezing? is used to avoid deep recursion. Discuss if there are any
+;; states, e.g. on exception, that might ruin this approach
+(defn- freeze
+  [system]
+  ;; only freeze the system if some other loop isn't marked as freezing the system
+  (if (:freezing? system)
+    system
+    (assoc
+        (loop [system (assoc system :freezing? true)]
+          (if (stabile? system)
+            system
+            (recur (cool system))))
+      :freezing? false)))
+
+(defn- all-propagators
+  [system]
+  (set (mapcat second (:propagators system))))
+
+(extend-type PropagatorSystem
+
+  PropagatorSystemProtocol
+
+  (add-value [this cell value]
+    (-> this
+        (disturb-system cell value)
+        freeze))
+
+  (get-value [this cell]
+    (get-in this [:values cell] nothing))
+
+  (add-propagator [this cells f]
+    (-> this
+        (update-in [:propagators] #(update-keys % cells conj f))
+        (update-in [:alert-queue] conj f)
+        freeze))
+
+  (stabile? [this]
+    (empty? (:alert-queue this)))
+
+  ;; If you are interested in values as the system ticks along, hook
+  ;; into cool
+  (cool [this]
+    (if-let [[f & t] (:alert-queue this)]
+      (f (assoc this :alert-queue t))
+      this))
+
+  (alert-all-propagators [this]
+    (-> this
+        (update-in [:alert-queue] concat (all-propagators this))
+        freeze)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn make-system
-  []
-  (PropagatorSystem.
-   {} {} (default-merge) (default-contradictory?) []))
+  ([]
+     (make-system (default-merge) (default-contradictory?)))
+  ([merge contradictory?]
+     (PropagatorSystem.
+      {} {} merge contradictory? [] false)))
+
+
+#_(with-pre-hook! #'add-value
+  (fn [system cell value]
+    (println "add-value" cell value)))
+
+#_(with-pre-hook! #'stabile?
+  (fn [system]
+    (println "stabile?" system)))
+
+#_(with-post-hook! #'stabile?
+  (fn [result]
+    (println "stabile? was" result)))
+
+#_(-> (make-system)
+    (add-value :foo :bar)
+    (add-value :foo :bar)
+    (add-value :baz 42)
+    (add-propagator [:foo]
+                    (fn [system]
+                      (add-value system :baz 42)))
+    all-propagators)
