9e9df0808e6ac821b69b98d1c5b6684d32a79566
Thomas Kristensen <thomas.kristensen@uswitch.com>
2013-09-08 13:50:54 +0100

    Remove dire dep.

--- project.clj
+++ project.clj
@@ -3,12 +3,7 @@
   :url "http://github.com/tgk/propaganda"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-
-                 ;; convenient for debugging
-                 [dire "0.4.4"]
-
-                 ]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :plugins [[lein-release "1.0.4"]]
   :lein-release {:scm :git
                  :deploy-via :shell
