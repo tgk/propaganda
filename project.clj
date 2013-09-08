c6330211f36a31552cfb7ff6d9ebeea2d004ee57
Thomas Kristensen <thomas.kristensen@uswitch.com>
2013-09-08 13:51:20 +0100

    lein-release plugin: preparing 0.0.3 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject propaganda "0.0.3-SNAPSHOT"
+(defproject propaganda "0.0.3"
   :description "(Hopefully) extensible propagator library"
   :url "http://github.com/tgk/propaganda"
   :license {:name "Eclipse Public License"
