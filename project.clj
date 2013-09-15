13366184d4c56de444567de8286de21ef012dfb2
Thomas Kristensen <thomas.kristensen@uswitch.com>
2013-09-15 11:58:59 +0100

    lein-release plugin: bumped version from 0.1.1 to 0.1.2-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject propaganda "0.1.1"
+(defproject propaganda "0.1.2-SNAPSHOT"
   :description "(Hopefully) extensible propagator library"
   :url "http://github.com/tgk/propaganda"
   :license {:name "Eclipse Public License"
