8ee1716d6a536c6c4153b33e9f8ca466362fe6f9
Thomas G. Kristensen <thomas.g.kristensen@gmail.com>
2013-09-27 08:03:16 +0100

    Bump to 0.2.0-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject propaganda "0.1.3-SNAPSHOT"
+(defproject propaganda "0.2.0-SNAPSHOT"
   :description "(Hopefully) extensible propagator library"
   :url "http://github.com/tgk/propaganda"
   :license {:name "Eclipse Public License"
