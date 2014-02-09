ea0198e57015ac5a015204fad8b235d179fc4673
Darrick Wiebe <dw@xnlogic.com>
2014-02-09 17:29:47 -0500

    Add supported values version of system intervals test

--- test/propaganda/system_intervals_test.clj
+++ test/propaganda/system_intervals_test.clj
@@ -21,24 +21,56 @@
             system
             (partition 2 shadow-height-pairs))))
 
-(deftest building-height-test
+(deftest system-building-height-test
   (let [system
         (-> (p/make-system (i/extend-merge (values/default-merge)) (values/default-contradictory?))
             (fall-duration :fall-time :building-height)
-            (similar-triangles :barometer-shadow
-                               :barometer-height
-                               :building-shadow
-                               :building-height)
+            (similar-triangles :barometer-shadow :barometer-height
+                               :building-shadow :building-height)
             (add-value :fall-time (make-interval 2.9 3.1))
             (add-value :building-shadow (make-interval 54.9 55.1))
             (add-value :barometer-height (make-interval 0.3 0.32))
             (add-value :barometer-shadow (make-interval 0.36 0.37))
-            #_(add-value :building-height 45.0) ; FIXME: get a contradiction if we don't use an interval
-            (add-value :building-height (make-interval 45.0 45.0))
-            )]
-    (is (= (make-interval 45.0 45.0) (get-value system :building-height)))
+            (add-value :building-height 45.0))]
+    (is (= 45.0 (get-value system :building-height)))
     (is (= (make-interval 54.9 55.1) (get-value system :building-shadow)))
     (is (= (make-interval 0.3 0.30327868852459017) (get-value system :barometer-height)))
     (is (= (make-interval 0.366 0.37) (get-value system :barometer-shadow)))
     (is (= (make-interval 3.025522031629098 3.0321598338046556) (get-value system :fall-time)))))
+
+(deftest system-building-height-with-support-test
+  (let [system
+        (-> (p/make-system (doto (values/default-merge) i/extend-merge sv/extend-merge)
+                           (doto (values/default-contradictory?) sv/extend-contradictory?))
+            (fall-duration :fall-time :building-height)
+            (similar-triangles :barometer-shadow :barometer-height
+                               :building-shadow :building-height)
+            (add-value :fall-time (supported (make-interval 2.9 3.3) :lousy-fall-time))
+            (add-value :building-shadow (supported (make-interval 54.9 55.1) :shadows))
+            (add-value :barometer-height (supported (make-interval 0.3 0.32) :shadows))
+            (add-value :barometer-shadow (supported (make-interval 0.36 0.37) :shadows))
+            (add-value :fall-time (supported (make-interval 2.9 3.1) :better-fall-time))
+            (add-value :building-height (supported 45.0 :superintendent)))]
+    (are [c v s] (and (is (= v (:value (get-value system c))))
+                      (is (= s (:support-set (get-value system c)))))
+
+         :building-height
+         45.0
+         #{:superintendent}
+
+         :building-shadow
+         (i/make-interval 54.9 55.1)
+         #{:superintendent :shadows}
+
+         :barometer-height
+         (i/make-interval 0.3 0.30327868852459017)
+         #{:superintendent :shadows :better-fall-time}
+
+         :barometer-shadow
+         (i/make-interval 0.366 0.37)
+         #{:superintendent :shadows :better-fall-time}
+
+         :fall-time
+         (i/make-interval  3.025522031629098 3.0321598338046556)
+         #{:superintendent})))
 
